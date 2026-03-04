ca9df86c2b9ba06a90f05cf327e00cd8da76c98b
ashoke-maity <ashokemaity092@gmail.com>
2026-03-04 19:21:09 +0530

    modified:   configs/db.js

--- configs/db.js
+++ configs/db.js
@@ -20,11 +20,6 @@ const connectDB = async () => {
         const EventRegData = await client`SELECT * FROM event_management.event_registrations`;
         const EventsData = await client`SELECT * FROM event_management.events`;
         const ParticipantsData = await client`SELECT * FROM event_management.participants`;
-        console.log("Successfully fetched participants data:", ParticipantsData);
-        console.log("Successfully fetched admin data:", AdminData);
-        console.log("Successfully fetched college representatives data:", CollegeRepData);
-        console.log("Successfully fetched Event Registration data:", EventRegData);
-        console.log("Successfully fetched Events data:", EventsData);
     } catch (error) {
         console.error("Database Error:", error);
     }
